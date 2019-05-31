c7eea1d5566994a522781304dfdc4e5751fff954
Melvin Carvalho <melvincarvalho@gmail.com>
2019-06-01 00:36:56 +0200

    change to a class

--- react/body.js
+++ react/body.js
@@ -1,10 +1,12 @@
-function Body(props) {
-    return (<section><h1>Brain Wallet</h1>
+
+
+  class Body extends React.Component {
+    render() { return <section><h1>Brain Wallet</h1>
       <hr/>
-      <input placeholder="I am a teapot!"/>
+      <input placeholder="I am a teapot"/>
       <hr/>
-      <input placeholder="Sha2 Hash of a teapot"/>
+      <input placeholder="Sha2 Hash of a teapot!"/>
       <hr/>
       </section>
-    )
+    }
   }
