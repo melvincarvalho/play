5e4c8c8834c1e5990c396dde10a6c87230d5c979
Melvin Carvalho <melvincarvalho@gmail.com>
2019-05-31 16:58:19 +0200

    DRY

---
+++ react/body.js
@@ -0,0 +1,10 @@
+function Body(props) {
+    return (<section><h1>Brain Wallet</h1>
+      <hr/>
+      <input placeholder="I am a teapot!"/>
+      <hr/>
+      <input placeholder="Sha2 Hash of a teapot"/>
+      <hr/>
+      </section>
+    )
+  }
